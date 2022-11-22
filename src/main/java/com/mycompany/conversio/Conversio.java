7317f01dd79fcde442aa2b83614a015246ef788b
DAM <DAM@203-12>
2022-11-22 18:18:18 +0100

    Canvi Menú de lliures a euros i de euros a lliures.

--- src/main/java/com/mycompany/conversio/Conversio.java
+++ src/main/java/com/mycompany/conversio/Conversio.java
@@ -16,10 +16,41 @@ public class Conversio {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Introdueix en £ ");
-        int valor_lliures = sc.nextInt();
+        int valor_lliures,valor_euros = 0;
+        double tipus_de_canvi = 0;
+        int opcio = 0;
+        
+        do{
+        
+                System.out.println("OPCIONS QUE POTS TRIAR");
+                System.out.println("1. Passar de Lliures a Euros");
+                System.out.println("2. Passar d'Euros a Lliures");
+                System.out.println("3. Sortir");
                 
-        double tipus_de_canvi = 1.15;
-        System.out.println(valor_lliures + " lliures esterlines (£) actualment equivalen a" + (valor_lliures * tipus_de_canvi) + " euros (€)");
-    }
+                System.out.println("\n Tria una opcio: ");
+                opcio = sc.nextInt();
+                
+                switch(opcio){
+                    case 1:
+                        System.out.println("Introdueix en £ ");
+                        valor_lliures = sc.nextInt();
+                
+                        tipus_de_canvi = 1.15;
+                        System.out.println(valor_lliures + " lliures esterlines (£) actualment equivalen a " + (valor_lliures * tipus_de_canvi) + " euros (€)");
+                 break;
+                    case 2:
+                        System.out.println("Introdueix en € ");
+                        valor_euros = sc.nextInt();
+                        
+                        tipus_de_canvi= 0.87;
+                        System.out.println(valor_euros + " euros (€) actualment equivalen a " + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+                  break;
+                    case 3:
+                        break;
+                        default:
+                        System.out.println("Opcio Incorrecta");
+                    }
+                }while (opcio != 3);
+  }
 }
+               
